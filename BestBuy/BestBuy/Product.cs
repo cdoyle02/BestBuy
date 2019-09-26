fbc3a6189e339dfcd493573c5879ea0a237c6c21
Charlie Doyle <charlesdoyle002@gmail.com>
2019-09-26 11:03:31 -0500

    added interface/class and instance of product

---
+++ BestBuy/BestBuy/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BestBuy
+{
+    class Product : IProduct
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public double PriceWithTax()
+        {
+            return Price *= 1.1;
+        }
+        
+    }
+}
---
+++ BestBuy/BestBuy/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BestBuy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var product = new Product();
+            product.Name = "Xbox";
+            product.Price = 250.00;
+
+            Console.WriteLine($"Name: {product.Name} Price: {product.Price} Price w/ tax: {product.PriceWithTax()}");
+        }
+    }
+}
